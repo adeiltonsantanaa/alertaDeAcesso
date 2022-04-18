61403b8dabe58ea287e16ff26e258cac3330ea18
adeiltonsantanaa <filhoaguiar20@gmail.com>
2022-04-18 15:01:10 -0300

    refatoração do código

--- AlertaDeAcessoAoPcpy.py
+++ AlertaDeAcessoAoPcpy.py
@@ -4,19 +4,20 @@ from email.mime.text import MIMEText
 from datetime import date, datetime
 import os
 
-user = (os.getlogin())
-
-if user != 'adeilton.filho':
+def pegar_hora_atual():
     data_atual = datetime.now()
-    data_em_texto = data_atual.strftime('%d/%m/%y')
-    hora_acesso = data_atual.strftime('%H:%M:%S')
-
+    data_em_texto = (f"dia {data_atual.strftime('%d/%m/%y')} exatamente às {data_atual.strftime('%H:%M:%S')}")
+    return data_em_texto
+def pegar_usuario_logado():
+    user = (os.getlogin())
+    return user
+def enviar_email():
     emailEnvio = "###SEU E-MAIL###"
     senhaEmailEnvio = "###CREDENCIAL SMTP###"
     enviarDe = "###E-MAIL QUE VAI ENVIAR###"
     enviarPara = "###E-MAIL QUE VAI RECEBER###"
     tituloEmail = "Alguém Acessou Seu PC"
-    corpoEmail = ("Olá, mestre. Como está o seu descanso?\nHoje dia {} exatamente às {} o seu PC foi acessado pelo usuário {}".format(data_em_texto, hora_acesso,user))
+    corpoEmail = ("Olá, mestre. Como está o seu descanso?\nHoje {} o seu PC foi acessado pelo usuário {}".format(pegar_hora_atual(), pegar_usuario_logado()))
 
     mimemsg = MIMEMultipart()
     mimemsg['From']=enviarDe
@@ -28,5 +29,10 @@ if user != 'adeilton.filho':
     connection.login(emailEnvio,senhaEmailEnvio)
     connection.send_message(mimemsg)
     connection.quit
+
+
+if pegar_usuario_logado() != 'adeilton.filho1':
+    enviar_email()
+      
 else:
     print("Usuário Autorizado!")
